417dbd5c5bd62c612840f0bb195b49c1cb4ea455
Kimmo <kimmo.j.taskinen@gmail.com>
2019-09-22 11:32:36 +0300

    Updated rendering for DateDetails

--- src/DateDetails.js
+++ src/DateDetails.js
@@ -11,38 +11,29 @@ const DateDetails = props => {
         const { tasksHistory } = value;
 
         // Miten käsittelen allaolevaa selectedDate-arrayta?
-        const selectedDate = tasksHistory.filter(
-          taskHistory => taskHistory.date === props.date
-        );
-        // Voin vaihtaa sen objektiksi esim
-        // const objekti = selectedDate[0]
-        // mutta en voi renderöidä siitä arvoja tyyliin objekti.dishwasher??
-        if (selectedDate[0]) {
-          console.log(selectedDate[0]);
-        }
+        const renderDate = tasksHistory
+          .filter(taskHistory => taskHistory.date === props.date)
+          .map(({ carbage, cooking, dishwasher, laundry, date }) => {
+            return (
+              <div className="task">
+                <h2 className="date">{date}</h2>
+                <p>
+                  Date:<span>{date}</span>
+                </p>
+                <p>
+                  Cooking:<span>{cooking}</span>
+                </p>
+                <p>
+                  Laundry:<span>{laundry}</span>
+                </p>
+                <p>
+                  Carbage & Bottles:<span>{carbage}</span>
+                </p>
+              </div>
+            );
+          });
 
-        return (
-          <div className="task">
-            <h2 className="date">{props.date}</h2>
-            <p>
-              {/* Allaoleva ratkaisu ei voi olla mitenkään järkevä, eihän? */}
-              {/* Kait noi saa mapattua jotenkin järkevämmin? */}
-              {/* DEMO: Klikkaa kalenterista joko: 20, 21, 22 niin ekalla rivillä näkyy miten pitäisi toimia  */}
-              {/* DEMO: Jos klikkaa jotain muuta niin crashaa, se korjataan myöhemmin :)  */}
-              {Object.keys(selectedDate[0])[1]}
-              <span>{Object.values(selectedDate[0])[1]}</span>
-            </p>
-            <p>
-              Cooking:<span>Jyrki</span>
-            </p>
-            <p>
-              Laundry:<span>Niina</span>
-            </p>
-            <p>
-              Carbage & Bottles:<span>Jyrki</span>
-            </p>
-          </div>
-        );
+        return renderDate;
       }}
     </Consumer>
   );
