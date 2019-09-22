fda2927a8ae47c5be579d6cf61197ad538b0c21b
Kimmo <kimmo.j.taskinen@gmail.com>
2019-09-22 22:59:37 +0300

    sketch

--- src/DateDetails.js
+++ src/DateDetails.js
@@ -8,41 +8,54 @@ const DateDetails = props => {
     <Consumer>
       {value => {
         const { tasksHistory } = value;
-        const arrayOfSelectedDate = tasksHistory.filter(
-          taskHistory => taskHistory.date === props.date
+        const renderEmpty = (
+          <div className="date-card">
+            <h2>{props.date}</h2>
+            <p>No data from this day.</p>
+          </div>
         );
-
-        if (arrayOfSelectedDate.length > 0) {
-          const objectOfSelectedDate = arrayOfSelectedDate[0];
-
-          const keys = Object.keys(objectOfSelectedDate).slice(1);
-          const values = Object.values(objectOfSelectedDate).slice(1);
-
-          const renderThis = keys.map((key, i) => {
-            const keyValuePaired = values[i];
-
-            return (
+        const renderDate = tasksHistory
+          .filter(taskHistory => taskHistory.date === props.date)
+          .map(selectedDate => {
+            return Object.entries(selectedDate).map(([key, value]) => (
               <p className="task" key={key}>
-                {key[0].toUpperCase()}
-                {key.slice(1)}
-                <span>{keyValuePaired}</span>
+                {key.toUpperCase()}
+                {value.slice(1)}
+                <span>
+                  `${key} ${value}`
+                </span>
               </p>
-            );
+            ));
           });
 
-          return (
-            <div className="date-card">
-              <h2>{props.date}</h2>
-              {renderThis}
-            </div>
-          );
-        } else
-          return (
-            <div className="date-card">
-              <h2>{props.date}</h2>
-              <p>No data from this day.</p>
-            </div>
-          );
+        console.log("render date", renderDate);
+        // if (arrayOfSelectedDate.length > 0) {
+        //   const objectOfSelectedDate = arrayOfSelectedDate[0];
+
+        //   const keys = Object.keys(objectOfSelectedDate).slice(1);
+        //   const values = Object.values(objectOfSelectedDate).slice(1);
+
+        //   const renderThis = keys.map((key, i) => {
+        //     const keyValuePaired = values[i];
+
+        //     return (
+        //       <p className="task" key={key}>
+        //         {key[0].toUpperCase()}
+        //         {key.slice(1)}
+        //         <span>{keyValuePaired}</span>
+        //       </p>
+        //     );
+        //   });
+
+        //   return (
+        //     <div className="date-card">
+        //       <h2>{props.date}</h2>
+        //       {renderThis}
+        //     </div>
+        //   );
+        // }
+
+        return renderDate.length ? renderDate : renderEmpty;
       }}
     </Consumer>
   );
